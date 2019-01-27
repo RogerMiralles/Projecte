ae8e2bd5c1bd1f7475b14ece33343f0a886f8129
Phosky <dioxyns@hotmail.com>
2019-01-27 14:38:36 +0100

    Puesto par que detecte el idioma del movil y traduzca la app a ese idioma, el button aun no funciona

--- app/src/main/java/com/example/miral/projecte/MainActivity.java
+++ app/src/main/java/com/example/miral/projecte/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.miral.projecte;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
@@ -11,7 +14,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    Locale myLocale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,4 +32,16 @@ public class MainActivity extends AppCompatActivity {
         Intent listSong = new Intent(this,rvi_lista_ejers.class);
         startActivity(listSong);
     }
+    public void onClickIdioma(View view){
+
+    }
+
+    public void setLocale(String lang){ 
+        Locale locale = new Locale("values-ca-rES");
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+        Intent n = new Intent();
+
+            }
 }
