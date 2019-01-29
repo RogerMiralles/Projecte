6be8f02acdfa86b50472ebb7ab246a51d01830fe
Phosky <dioxyns@hotmail.com>
2019-01-29 08:54:39 +0100

    Puesto mas bonito y modificados algunos intent

--- app/src/main/java/com/example/miral/projecte/Registre.java
+++ app/src/main/java/com/example/miral/projecte/Registre.java
@@ -2,6 +2,7 @@ package com.example.miral.projecte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Registre extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Registre extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registre);
+    }
+    public void onClick(View view){
+
     }
 }
