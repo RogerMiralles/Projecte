5e751a8612cc631e1794a3152012e1851548dccf
RogerMiralles <miralles.monfort@gmail.com>
2019-01-29 08:48:32 +0100

    canvios

--- app/src/main/java/com/example/miral/projecte/calendar.java
+++ app/src/main/java/com/example/miral/projecte/calendar.java
@@ -2,6 +2,8 @@ package com.example.miral.projecte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class calendar extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class calendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+    }
+
+    public void data(View view){
+        Toast.makeText(this,"work in progres",Toast.LENGTH_SHORT);
     }
 }
