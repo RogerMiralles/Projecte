dd3bef30f34f745ffacced184ed4b3d2e2468d51
Phosky <dioxyns@hotmail.com>
2019-02-25 12:53:45 +0100

    Añadidos los textView para verificar el registro

--- app/src/main/java/com/example/miral/projecte/Registre.java
+++ app/src/main/java/com/example/miral/projecte/Registre.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 public class Registre extends AppCompatActivity {
 
     TextView usuario,email,pass1,pass2,nombre, apellidos;
+    TextView eusuario, eemail, epass1, enombre, eapellidos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +21,36 @@ public class Registre extends AppCompatActivity {
         pass2 = findViewById(R.id.etxtPass2);
         nombre = findViewById(R.id.etxtNombre);
         apellidos = findViewById(R.id.etxtApellidos);
+        //errores
+        eusuario = findViewById(R.id.txtErrorUsu);
+        eemail = findViewById(R.id.txtCorreoNoValido);
+        epass1 = findViewById(R.id.txtErrorContraseña);
+        enombre = findViewById(R.id.TxtnombreError);
+        eapellidos = findViewById(R.id.txtApellidosError);
+
+    }
+    public void onClickLog(View view){
+        this.finish();
     }
     public void onClick(View view){
         Boolean temp = true;
-        String usuarioRegistro = (String) usuario.getText();
-        
+        String usuarioRegistro;
+
+        eusuario.setVisibility(View.VISIBLE);
+        eusuario.setText("Nombre ya en uso.");
+        eemail.setVisibility(View.VISIBLE);
+        eemail.setText("Correo en uso");
+        epass1.setVisibility(View.VISIBLE);
+        epass1.setText("Las contraseñas no coinciden");
+        enombre.setVisibility(View.VISIBLE);
+        enombre.setText("El nombre no puede contener numeros");
+        eapellidos.setVisibility(View.VISIBLE);
+        eapellidos.setText("los apellidos no pueden contener numeros");
 
 
 
-        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+//        startActivity(intent);
     }
     public boolean compruebanom(){
                 /*
