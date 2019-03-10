e4f295dadbeed882de85cab04ab5bd74a2cdfa7b
Phosky <dioxyns@hotmail.com>
2019-03-10 18:20:21 +0100

    Eliminados los 3 puntos del menu

--- app/src/main/java/com/example/miral/projecte/navegacion.java
+++ app/src/main/java/com/example/miral/projecte/navegacion.java
@@ -54,8 +54,9 @@ public class navegacion extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navegacion, menu);
-        return true;
+
+        // getMenuInflater().inflate(R.menu.navegacion, menu);
+        return false;
     }
 
     @Override
