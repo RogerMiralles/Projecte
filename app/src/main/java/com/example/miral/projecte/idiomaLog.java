db06272c9060cf8626ccfd1b242e514a2753dcab
RogerMiralles <miralles.monfort@gmail.com>
2019-03-10 17:15:28 +0100

    idioma

--- app/src/main/java/com/example/miral/projecte/idiomaLog.java
+++ app/src/main/java/com/example/miral/projecte/idiomaLog.java
@@ -5,21 +5,13 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.miral.projecte.fragments.idioma;
-
-import java.util.LinkedList;
 import java.util.Locale;
 
 public class idiomaLog extends AppCompatActivity {
@@ -29,6 +21,9 @@ public class idiomaLog extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         loadLocale();
         setContentView(R.layout.activity_idioma_log);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle(getResources().getString(R.string.app_name));
 
         Button changeLang=findViewById(R.id.chngeMyLang);
         changeLang.setOnClickListener(new View.OnClickListener() {
@@ -46,7 +41,7 @@ public class idiomaLog extends AppCompatActivity {
         mBuilder.setTitle("Elige Idioma.....");
         mBuilder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int i) {
+            public void onClick(DialogInterface dialogInterface, int i) {
                 if(i==0){
                     Toast.makeText(getApplicationContext(), "Traduciendo al castellano", Toast.LENGTH_SHORT).show();
                     setLocale("ca-rES");
@@ -62,33 +57,12 @@ public class idiomaLog extends AppCompatActivity {
                     setLocale("en");
                     recreate();
                 }
-                dialog.dismiss();
+                dialogInterface.dismiss();
             }
         });
         AlertDialog mDialog=mBuilder.create();
         mDialog.show();
     }
-
-/*
-    public void onItemClick(View view,int position){
-            switch(position){
-                case 0:
-                    Toast.makeText(getApplicationContext(), "Traduciendo al castellano", Toast.LENGTH_SHORT).show();
-                    setLocale("ca-rES");
-                    recreate();
-                    break;
-                case 1:
-                    Toast.makeText(getApplicationContext(), "Traduciendo al catalan", Toast.LENGTH_SHORT).show();
-                    setLocale("ca");
-                    recreate();
-                    break;
-                case 2:
-                    Toast.makeText(getApplicationContext(), "Traduciendo al ingles", Toast.LENGTH_SHORT).show();
-                    setLocale("en");
-                    recreate();
-                    break;
-            }
-        }*/
 
         private void setLocale(String s) {
             Locale locale=new Locale(s);
@@ -107,9 +81,4 @@ public class idiomaLog extends AppCompatActivity {
             setLocale(language);
         }
 
-
-    /*private void CargarFragmente(Fragment fragment){
-        FragmentManager manager=getSupportFragmentManager();
-        manager.beginTransaction().replace(R.id.contendedorFragmentoI,fragment).commit();
-    }*/
 }
